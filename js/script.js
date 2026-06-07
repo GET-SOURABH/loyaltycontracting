8da4e4c89bbb11fb90370b6e6deb326ae37c7c02
sourabh <sourabh@sourabhs-MacBook-Air.local>
2026-06-07 15:33:26 +0530

    Local website version

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const tabButtons = document.querySelectorAll(".tabs button");
+
+tabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    tabButtons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+  });
+});
