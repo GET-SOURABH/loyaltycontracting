055dfab572647538fa6fd77065a137b1b53d5c20
GET-SOURABH <sourabh55jan@gmail.com>
2026-06-08 12:31:21 +0530

    old vs new feature added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ tabButtons.forEach((button) => {
     button.classList.add("active");
   });
 });
+
+
+// Before / After sliders
+const beforeAfterCards = document.querySelectorAll(".before-after-card");
+
+beforeAfterCards.forEach((card) => {
+  const slider = card.querySelector(".ba-range");
+
+  slider.addEventListener("input", () => {
+    card.style.setProperty("--position", slider.value + "%");
+  });
+});
